8b27ad8f8bf43558cded8469852c0faaf81b4257
DuciucD <duciuc_daniel@ymail.com>
2022-01-23 15:14:05 +0200

    Add project files.

---
+++ Duciuc_Daniel_Lab12_Mobile/Duciuc_Daniel_Lab12_Mobile/Data/IRestService.cs
@@ -0,0 +1,17 @@
+﻿using Duciuc_Daniel_Lab12_Mobile.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Duciuc_Daniel_Lab12_Mobile.Data
+{
+
+        public interface IRestService
+        {
+            Task<List<ShopList>> RefreshDataAsync();
+            Task SaveShopListAsync(ShopList item, bool isNewItem);
+            Task DeleteShopListAsync(int id);
+        }
+    
+}
---
+++ Duciuc_Daniel_Lab12_Mobile/Duciuc_Daniel_Lab12_Mobile/Models/ShopList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Duciuc_Daniel_Lab12_Mobile.Models
+{
+    public class ShopList
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
